ace5ef76a31c2b792bc2fa80573e4c46a17a4a3d
Daniel Stonier <d.stonier@gmail.com>
2019-04-01 14:54:46 -0400

    add foo

---
+++ foo/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# License: BSD
+#   https://raw.github.com/splintered-reality/py_trees_ros/license/LICENSE
+#
+##############################################################################
+# Documentation
+##############################################################################
+
+"""
+ROS extensions, behaviours and utilities for py_trees.
+"""
+
+##############################################################################
+# Imports
+##############################################################################
+
+from . import utilities
